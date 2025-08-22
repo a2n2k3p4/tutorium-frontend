16677e56c1f15c116ec173bb639954aee4d96e1f
alongkot <alongkotwimonsuk@gmail.com>
2025-08-22 15:04:51 +0700

    feat: add bottom navigation bar

---
+++ lib/pages/profile/profile_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Padding(
+          padding: const EdgeInsets.only(top: 30),
+          child: Text("Profile"),
+        ),
+        titleTextStyle: const TextStyle(
+          color: Colors.black,
+          fontSize: 36.0,
+          fontWeight: FontWeight.normal,
+        ),
+      ),
+      body: Center(child: Text("Profile Page")),
+    );
+  }
+}
---
+++ lib/pages/search/search_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatelessWidget {
+  const SearchPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Padding(
+          padding: const EdgeInsets.only(top: 30),
+          child: Text("Search"),
+        ),
+        titleTextStyle: const TextStyle(
+          color: Colors.black,
+          fontSize: 36.0,
+          fontWeight: FontWeight.normal,
+        ),
+      ),
+      body: Center(child: Text("Search Page")),
+    );
+  }
+}
