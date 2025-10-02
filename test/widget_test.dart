b1144993fbab544987374d1e02fdc9b69a8016f3
Athiphu.p <athiphu.p@ku.th>
2025-10-02 20:15:39 +0700

    chore: add package flutter_dotenv

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:tutorium_frontend/main.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:tutorium_frontend/pages/main_nav_page.dart';
 import 'package:tutorium_frontend/pages/widgets/schedule_card_learner.dart';
 
